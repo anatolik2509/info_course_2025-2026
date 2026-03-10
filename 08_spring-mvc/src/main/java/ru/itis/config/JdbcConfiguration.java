ad0396dbf402fba5830050121392ce56e9a37ebe
anatoly <antonov250901@mail.ru>
2026-03-10 23:30:38 +0300

    started adding jdbc

---
+++ 08_spring-mvc/src/main/java/ru/itis/config/JdbcConfiguration.java
@@ -0,0 +1,31 @@
+package ru.itis.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class JdbcConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        return new DriverManagerDataSource("url", "user", "pass"); //todo set
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:config/liquibase/master.xml"); //todo set
+        liquibase.setDataSource(dataSource());
+        return liquibase;
+    }
+}
